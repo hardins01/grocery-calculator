9438fc642d600ee8da2874077152210b47707b56
Sebastian Hardin <sebastian.hardin@gmail.com>
2022-05-20 22:15:35 -0700

    Prep for integration of venmo transactions.

--- config.py
+++ config.py
@@ -25,67 +25,71 @@ CREDENTIALS = [
 ]
 
 
-# quit if no command line argument was given
-try:
-     command = sys.argv[1]
-except IndexError:
-     print("Error: no command-line argument was given\nUsage:    python config.py venmo-init    or    python config.py venmo-logout")
-     quit()
+def main():
+     # quit if no command line argument was given
+     try:
+          command = sys.argv[1]
+     except IndexError:
+          print("Error: no command-line argument was given\nUsage:    python config.py venmo-init    or    python config.py venmo-logout")
+          return
 
 
-# quit if an invalid command line argument was given
-if len(sys.argv) > 2 or (command != "venmo-init" and command != "venmo-logout"):
-     print("Error: invalid command-line argument given\nUsage:    python config.py venmo-init    or    python config.py venmo-logout")
-     quit()
+     # quit if an invalid command line argument was given
+     if len(sys.argv) > 2 or (command != "venmo-init" and command != "venmo-logout"):
+          print("Error: invalid command-line argument given\nUsage:    python config.py venmo-init    or    python config.py venmo-logout")
+          return
 
 
-# if the user wants to initialize credentials
-if command == "venmo-init":
-     # first check that the file isn't empty
-     if os.stat("access_tokens.json").st_size != 0:
-          print("Error: access tokens already initialized, clear them first with    python config.py venmo-logout")
-          quit()
+     # if the user wants to initialize credentials
+     if command == "venmo-init":
+          # first check that the file isn't empty
+          if os.stat("access_tokens.json").st_size != 0:
+               print("Error: access tokens already initialized, clear them first with    python config.py venmo-logout")
+               return
 
-     print("\n============ begin 'venmo-api' messages ============\n")
-     
-     # proceed to create the access tokens for whoever provided credentials
-     access_tokens = {}
-     for cred in CREDENTIALS:
-          if cred[1] != "" and cred[2] != "":
-               print("Generating access token for {}".format(cred[0]))
-               access_tokens.update({cred[0]: Client.get_access_token(username=cred[1], password=cred[2])})
-          else:
-               access_tokens.update({cred[0]: ""})
-     
-     print("\n============= end 'venmo-api' messages =============\n")
+          print("\n============ begin 'venmo-api' messages ============\n")
+          
+          # proceed to create the access tokens for whoever provided credentials
+          access_tokens = {}
+          for cred in CREDENTIALS:
+               if cred[1] != "" and cred[2] != "":
+                    print("Generating access token for {}".format(cred[0]))
+                    access_tokens.update({cred[0]: Client.get_access_token(username=cred[1], password=cred[2])})
+               else:
+                    access_tokens.update({cred[0]: ""})
+          
+          print("\n============= end 'venmo-api' messages =============\n")
 
-     with open("access_tokens.json", "w") as file:
-          json.dump(access_tokens, file)
-     
-     quit()
-     
+          with open("access_tokens.json", "w") as file:
+               json.dump(access_tokens, file)
+          
+          return
+          
 
-# if the user wants to log out and clear their credentials
-if command == "venmo-logout":
-     # first check that the file has contents to clear
-     if os.stat("access_tokens.json").st_size == 0:
-          print("Error: no access tokens to log out with, generate them with    python config.py venmo-init")
-          quit()
+     # if the user wants to log out and clear their credentials
+     if command == "venmo-logout":
+          # first check that the file has contents to clear
+          if os.stat("access_tokens.json").st_size == 0:
+               print("Error: no access tokens to log out with, generate them with    python config.py venmo-init")
+               return
 
-     print("\n============ begin 'venmo-api' messages ============\n")
-     
-     # proceed to log out of the access tokens
-     access_tokens = {}
-     with open("access_tokens.json", "r") as file:
-          access_tokens = json.load(file)
-     for token in access_tokens.values():
-          if token != "":
-               client = Client(access_token=token)
-               client.log_out(token)
-     
-     print("\n============= end 'venmo-api' messages =============\n")
+          print("\n============ begin 'venmo-api' messages ============\n")
+          
+          # proceed to log out of the access tokens
+          access_tokens = {}
+          with open("access_tokens.json", "r") as file:
+               access_tokens = json.load(file)
+          for token in access_tokens.values():
+               if token != "":
+                    client = Client(access_token=token)
+                    client.log_out(token)
+          
+          print("\n============= end 'venmo-api' messages =============\n")
 
-     # clear the contents of the file
-     open('access_tokens.json', 'w').close()
+          # clear the contents of the file
+          open('access_tokens.json', 'w').close()
 
-     quit()
+          return
+
+if __name__ == "__main__":
+     main()
--- grocery.py
+++ grocery.py
@@ -1,141 +1,203 @@
 # import the grocery_manager class from the other file
 from grocery_manager import Grocery_Manager
-from config import INITIALS
+from config import INITIALS, CREDENTIALS
 
-# print the introduction message
-print("Hello, Welcome to Grocery-Calculator!         By Sebastian Hardin")
-print("Enter 'h' or 'help' for a list of valid commands")
 
-# create the grocery_manager
-groc_man = Grocery_Manager(INITIALS)
+def main():
+     # create the grocery_manager
+     groc_man = Grocery_Manager(INITIALS)
 
-# the main control loop of the program
-while 1:
-     
-     # get the user's input
-     print("> ", end="")
-     user_input = input()
+     # define all valid versions of each command
+     add_commands = ["a", "add"]
+     delete_commands = ["d", "drop", "delete"]
+     receipt_commands = ["r", "receipt", "ls"]
+     help_commands = ["h", "help"]
+     quit_commands = ["s", "stop", "e", "exit", "q", "quit"]
+     confirmation_commands = ["y", "yes", "n", "no"]
+     venmo_commands = ["v", "venmo"]
 
-     # split every token (string of consecutive non-whitespaces)
-     # user_input[0] is the command the user gave, followed by the parameters
-     user_input = user_input.split()
-     command = user_input[0]
 
-     if command == "a" or command == "add":      # a or add command
+     # print the introduction message
+     print("Hello, Welcome to Grocery-Calculator!         By Sebastian Hardin")
+     print("Enter 'h' or 'help' for a list of valid commands")
+
+
+     # the main control loop of the program
+     keep_looping = True
+     while keep_looping:
           
-          # check the number of parameters
-          if len(user_input) < 3:
-               print("Error: invalid input for 'add' command (see 'help' for details)")
-          else:
-               # get the price, checking for error
-               price = 0.0
-               valid_price = True
-               try:
-                    price = float(user_input[1])
-               except ValueError as e:
+          # get the user's input
+          print("> ", end="")
+          user_input = input()
+
+          # split every token (string of consecutive non-whitespaces)
+          # user_input[0] is the command the user gave, followed by the parameters
+          user_input = user_input.split()
+          command = user_input[0]
+
+          if command in add_commands:      # a or add command
+               
+               # check the number of parameters
+               if len(user_input) < 3:
                     print("Error: invalid input for 'add' command (see 'help' for details)")
-                    valid_price = False
-               
-               if valid_price:
-                    # get the initials, which can be many tokens long
-                    initials = ""
-                    for i in range(2, len(user_input)):
-                         initials += user_input[i]
-                    initials = "".join(initials.split())
-
-                    # verify the initials before attempting the entry
-                    valid_initials = groc_man.verify_initials(initials)
-                    if valid_initials == 0:
-                         print("Error: invalid initials given")
-                    else:
-                         # create a new entry in the grocery manager
-                         entry_id = groc_man.new_entry(price, initials)
-
-                         # confirm the entry, printing its id for reference
-                         print("ID#{}\t$ {} {}".format(entry_id, "{:.2f}".format(price), initials))
-
-     elif command == "d" or command == "delete" or command == "drop":       # d or delete/drop command
-
-          # check if the next token is "p", "prev", or "previous", in which case drop the most recent entry
-          if user_input[1] == "p" or user_input[1] == "prev" or user_input[1] == "previous":
-               
-               # drop the previous entry
-               return_code = groc_man.delete_prev_entry()
-
-               # check the return code
-               if return_code == 0 or return_code == -1:
-                    print("Error: no previous entry to delete")
                else:
-                    print("Success: previous entry has been deleted")
-               
-          else:     # the user wants to delete a specific entry
+                    # get the price, checking for error
+                    price = 0.0
+                    valid_price = True
+                    try:
+                         price = float(user_input[1])
+                    except ValueError as e:
+                         print("Error: invalid input for 'add' command (see 'help' for details)")
+                         valid_price = False
+                    
+                    if valid_price:
+                         # get the initials, which can be many tokens long
+                         initials = ""
+                         for i in range(2, len(user_input)):
+                              initials += user_input[i]
+                         initials = "".join(initials.split())
 
-               # check if the entry id given is valid
-               entry_id = 0
-               valid_id = True
-               try:
-                    entry_id = int(user_input[1])
-               except ValueError as e:
-                    print("Error: invalid input for 'delete'/'drop' command (see 'help' for details)")
-                    valid_id = False
-               
-               # continue on if the given id is an integer
-               if valid_id:
-                    return_code = groc_man.delete_entry(entry_id)
+                         # verify the initials before attempting the entry
+                         valid_initials = groc_man.verify_initials(initials)
+                         if valid_initials == 0:
+                              print("Error: invalid initials given")
+                         else:
+                              # create a new entry in the grocery manager
+                              entry_id = groc_man.new_entry(price, initials)
+
+                              # confirm the entry, printing its id for reference
+                              print("ID#{}\t$ {} {}".format(entry_id, "{:.2f}".format(price), initials))
+
+          elif command in delete_commands:       # d or delete/drop command
+
+               # check if the next token is "p", "prev", or "previous", in which case drop the most recent entry
+               if user_input[1] == "p" or user_input[1] == "prev" or user_input[1] == "previous":
+                    
+                    # drop the previous entry
+                    return_code = groc_man.delete_prev_entry()
 
                     # check the return code
-                    if return_code == 0:
-                         print("Error: entry id given is outside the valid range of entry id's")
-                    elif return_code == -1:
-                         print("Error: entry has already been deleted")
+                    if return_code == 0 or return_code == -1:
+                         print("Error: no previous entry to delete")
                     else:
-                         print("Success: entry ID#{} has been deleted".format(entry_id))
+                         print("Success: previous entry has been deleted")
+                    
+               else:     # the user wants to delete a specific entry
+
+                    # check if the entry id given is valid
+                    entry_id = 0
+                    valid_id = True
+                    try:
+                         entry_id = int(user_input[1])
+                    except ValueError as e:
+                         print("Error: invalid input for 'delete'/'drop' command (see 'help' for details)")
+                         valid_id = False
+                    
+                    # continue on if the given id is an integer
+                    if valid_id:
+                         return_code = groc_man.delete_entry(entry_id)
+
+                         # check the return code
+                         if return_code == 0:
+                              print("Error: entry id given is outside the valid range of entry id's")
+                         elif return_code == -1:
+                              print("Error: entry has already been deleted")
+                         else:
+                              print("Success: entry ID#{} has been deleted".format(entry_id))
 
 
-     
-     elif command == "r" or command == "receipt" or command == "ls":           # r or receipt or ls command
+          
+          elif command in receipt_commands:           # r or receipt or ls command
 
-          # validate the rest of the input is valid
-          if len(user_input) != 1:
-               print("Error: invalid input for 'r'/'ls'/'receipt' command (see 'help' for details)")
+               # validate the rest of the input is valid
+               if len(user_input) != 1:
+                    print("Error: invalid input for 'r'/'ls'/'receipt' command (see 'help' for details)")
+               else:
+                    # print all active non-deleted entries and current subtotals
+                    groc_man.print_status()
+
+          elif command in help_commands:         # h or help command
+
+               # print all valid commands
+               print(
+                    "\n"\
+                    "\t\tAll valid commands for Grocery-Calculator:\n\n\n"\
+                    "\ta / add : Add a new item to the grocery run\n\n"\
+                    "\t\t> { a | add } PRICE INITIALS\n\n"\
+                    "\t\tPRICE = the price of the item, must be convertable to a float\n"\
+                    "\t\tINITIALS = the initials of the people paying for this item\n"\
+                    "\t\t\tnote: an initial can appear multiple times to achieve the desired ratio of cost per person\n\n"\
+                    "\tr / receipt / ls : Print the currently active items, along with everyone's subtotals\n\n"\
+                    "\t\t> { r | ls | receipt }\n\n"\
+                    "\td / drop / delete : Delete a specific item or the most recently added active item\n\n"\
+                    "\t\t> { d | drop | delete } { ENTRY_ID | { p | prev | previous } }\n\n"\
+                    "\t\tENTRY_ID = the id of the item to be deleted, must be a valid id that hasn't already been deleted\n\n"\
+                    "\tq / quit / s / stop / e / exit : Exit the program, printing everyone's totals that they owe\n\n"\
+                    "\t\t> { q | s | e | quit | stop | exit }\n\n"\
+                    "\th / help : Bring up this menu, a list of all available commands to use\n\n"\
+                    "\t\t> { h | help }\n\n"
+               )
+
+          elif command in quit_commands:          # s or stop or e or exit or q or quit command
+
+               # check if the user wants to end usage with venmo transaction(s)
+               if len(user_input) == 1:
+                    confirmation = ""
+                    while confirmation not in confirmation_commands:
+                         print("Are you sure you want to stop running Grocery-Calculator, confirming your entries?")
+                         print("You won't be able to go back and change them, but the results will be printed one more time for you as the program quits")
+                         print("(y/n) > ", end="")
+                         confirmation = input()
+                         if confirmation not in confirmation_commands:
+                              print("Invalid input, must be 'y', 'yes', 'n', or 'no'")
+                    
+                    # exit the main control loop if the user wants to be done (just keep going as normal otherwise)
+                    if confirmation == "y" or confirmation == "yes":
+                         keep_looping = False
+
+               else:
+                    if user_input[1] in venmo_commands:
+                         confirmation = ""
+                         while confirmation not in confirmation_commands:
+                              print("Are you sure you want to stop running Grocery-Calculator, confirming your entries and initiating Venmo requests?")
+                              print("(y/n) > ", end="")
+                              confirmation = input()
+                              if confirmation not in confirmation_commands:
+                                   print("Invalid input, must be 'y', 'yes', 'n', or 'no'")
+
+                         # initiate venmo actions if the user said yes
+                         if confirmation == "y" or confirmation == "yes":
+                              payer = " "
+                              while payer not in INITIALS:
+                                   print("Who paid at the grocery store? Your response MUST be one of the initials provided in config.py")
+                                   print("> ", end="")
+                                   payer = input()
+                                   if len(payer) != 1 or payer not in INITIALS:
+                                        print("Invalid input, must be one of the people provided in config.py")
+                              
+                              payer_index = INITIALS.find(payer)
+                              
+                              print("payer: {} {}".format(payer, CREDENTIALS[payer_index][0]))
+
+                              # INSERT VENMO LOGIC HERE
+
+
+                              keep_looping = False
+
+                    else:
+                         print("Error: invalid input for 'quit'/'stop'/'exit' command (see 'help' for details)")
+
           else:
-               # print all active non-deleted entries and current subtotals
-               groc_man.print_status()
 
-     elif command == "h" or command == "help":         # h or help command
+               # tell the user their input was invalid
+               print("Error: command '{}' not found (see 'help' for details)".format(command))
+          
 
-          # print all valid commands
-          print(
-               "\n"\
-               "\t\tAll valid commands for Grocery-Calculator:\n\n\n"\
-               "\ta / add : Add a new item to the grocery run\n\n"\
-               "\t\t> { a | add } PRICE INITIALS\n\n"\
-               "\t\tPRICE = the price of the item, must be convertable to a float\n"\
-               "\t\tINITIALS = the initials of the people paying for this item\n"\
-               "\t\t\tnote: an initial can appear multiple times to achieve the desired ratio of cost per person\n\n"\
-               "\tr / receipt / ls : Print the currently active items, along with everyone's subtotals\n\n"\
-               "\t\t> { r | ls | receipt }\n\n"\
-               "\td / drop / delete : Delete a specific item or the most recently added active item\n\n"\
-               "\t\t> { d | drop | delete } { ENTRY_ID | { p | prev | previous } }\n\n"\
-               "\t\tENTRY_ID = the id of the item to be deleted, must be a valid id that hasn't already been deleted\n\n"\
-               "\tq / quit / s / stop / e / exit : Exit the program, printing everyone's totals that they owe\n\n"\
-               "\t\t> { q | s | e | quit | stop | exit }\n\n"\
-               "\th / help : Bring up this menu, a list of all available commands to use\n\n"\
-               "\t\t> { h | help }\n\n"
-          )
+     # print the results
+     print("")
+     groc_man.print_results()
+     print("\nThank you for using Grocery-Calculator!")
 
-     elif command == "s" or command == "stop" or command == "e" or command == "exit" or command == "q" or command == "quit":          # s or stop or e or exit or q or quit command
 
-          # exit from the while loop
-          break
-
-     else:
-
-          # tell the user their input was invalid
-          print("Error: command '{}' not found (see 'help' for details)".format(command))
-     
-
-# print the results
-print("")
-groc_man.print_results()
-print("\nThank you for using Grocery-Calculator!")
+if __name__ == "__main__":
+     main()
